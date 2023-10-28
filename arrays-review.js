60a163d9ae0cb158039c5c15a5f1af8e54f60a4c
Erik Mellum <erikmellum@eriks-mbp.lan>
2023-10-27 21:31:01 -0700

    first commit

---
+++ arrays-review.js
@@ -0,0 +1,25 @@
+// 2.0 create an array with no elements inside of it, log the array
+
+// 2.1 use array.push to add a number to the array, log the array
+
+// 2.2 create a new array with a number already inside of it, log the array
+
+// 2.3 use array.pop to remove an element from the back of the array
+
+// 2.4 push two more numbers into your array
+
+// 2.5 use spread syntax to combine arrays from above problems, log the results
+
+// 2.6 use array.find to locate one of the elements in your array, log the located result
+
+// 2.7 use array.every to determine if every single number is above "5", log the result
+
+// 2.8 use array.filter to remove any numbers smaller than 5, log the result
+
+// 2.9 use array.sort to sort the items from smallest to largest
+
+// 2.91 use array.reverse to reverse the items so that they are now largest to smallest
+
+// 2.92 use array.reduce to sum up the entire list of numbers in the array
+
+// 2.93 use array.map to scale each number in the array up by a factor of 10
---
+++ objects-review.js
@@ -0,0 +1,13 @@
+// 1.0 object shorthand, create object with properties title, description
+
+// 1.1 property access, use "dot" to console.log the title property
+
+// 1.2 property access, use "square brackets" to console.log the description property
+
+// 1.3 getting all keys on an object, use Object.keys to get the list of keys on the object. Log the keys
+
+// 1.4 getting all the values on an object, use Object.values to get list of values on the object. Log the values
+
+// 1.5 destructuring - take title and description property out using destructuring. Log each of them.
+
+// 1.6 spread syntax - create a new object that includes properties from the above object, plus one additional property
